b6013d24f89858cac519d48663e14613b902c4de
Riyan Sugiarto <ryansgrt23@gmaik.com>
2022-12-21 15:44:57 +0700

    Spring Cloud API Gateway

--- api-gateway/src/test/java/com/riyan/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/riyan/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.infybuzz.app;
+package com.riyan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- eureka-server/src/test/java/com/riyan/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/riyan/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.riyan.microservicepractice;
+package com.riyan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroservicePracticeApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
